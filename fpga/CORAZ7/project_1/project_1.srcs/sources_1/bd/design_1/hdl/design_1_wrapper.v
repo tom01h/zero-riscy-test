788a9c5da734ce9d0f6e697f559c113312993fc2
tom01h <tom01h@gmail.com>
2018-07-07 19:51:34 +0900

    add Cora Z7

--- fpga/CORAZ7/project_1/project_1.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ fpga/CORAZ7/project_1/project_1.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
-//Date        : Sat Jul  7 17:07:04 2018
+//Date        : Sat Jul  7 18:31:07 2018
 //Host        : DESKTOP-SLOBRID running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
